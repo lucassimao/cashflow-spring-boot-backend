00ff2730f1766f156938db02c91d2d6b67b5ecdf
Lucas Simao <Lucas Simao>
2018-12-26 16:59:22 -0300

    1st commit

---
+++ src/test/java/com/lucassimao/fluxodecaixa/FluxoDeCaixaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lucassimao.fluxodecaixa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FluxoDeCaixaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
