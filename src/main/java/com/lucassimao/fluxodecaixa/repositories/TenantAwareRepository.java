15b982c675bbb488fd01a0f9f7971b435f7026f5
Lucas Simao <Lucas Simao>
2019-03-23 12:12:13 -0300

    updating javadoc

--- src/main/java/com/lucassimao/fluxodecaixa/repositories/TenantAwareRepository.java
+++ src/main/java/com/lucassimao/fluxodecaixa/repositories/TenantAwareRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Interface created in order to mark a repository that must be 
- * augmented with the {@link com.lucassimao.fluxodecaixa.aspect.BookEntryGroupRepositoryAspect} aspect.
+ * augmented with the {@link com.lucassimao.fluxodecaixa.aspect.TenantFilterActivatorAspect} aspect.
  * The requirement is that the implementing class must provide the current persistence context throgh the method getEntityManager
  */
 public interface TenantAwareRepository {
